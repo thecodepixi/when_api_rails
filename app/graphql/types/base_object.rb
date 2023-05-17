a75d1d7c11a258b866763a8f50a1af2cf0a319a0
emmy <emily.a.harber@gmail.com>
2023-05-17 15:21:16 -0400

    Add configuration for node interface

--- app/graphql/types/base_object.rb
+++ app/graphql/types/base_object.rb
@@ -1,5 +1,6 @@
 module Types
   class BaseObject < GraphQL::Schema::Object
+    implements GraphQL::Types::Relay::Node
     edge_type_class(Types::BaseEdge)
     connection_type_class(Types::BaseConnection)
     field_class Types::BaseField
